8a900572aa09523d8b7fa07658213acd9dd50b7a
ryanoneill1 <ryanoneill1@gmail.com>
2026-08-21 17:34:11 -0500

    Add files via upload

--- scripts/validate-search-urls.ts
+++ scripts/validate-search-urls.ts
@@ -11,7 +11,7 @@
 
 import { AFFILIATE } from '../src/data/affiliate-links';
 
-const THRESHOLD = 157;
+const THRESHOLD = 151;
 
 const search = Object.entries(AFFILIATE as any)
   .filter(([, v]) => /\/s\?/.test(String((v as any).url)))
